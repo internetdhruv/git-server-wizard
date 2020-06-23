6ac588173dd85d33d9f455937735cc9709839015
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-06-23 22:30:43 +0400

    Final instructions added

--- git-server-wizard.sh
+++ git-server-wizard.sh
@@ -82,7 +82,7 @@ echo "[git-server-wizard] Setting up nginx..."
 echo "[git-server-wizard] Creating git http backend for git clone"
 sed 's/<<<SOURCE_CONFIG_HERE>>>/'$ESCAPED_HOME'/' ./git-http-backend.conf > ./git-http-backend.conf.temp
 [ -d "/etc/nginx" ] && mv ./git-http-backend.conf.temp /etc/nginx/git-http-backend.conf
-[ -d "/etc/nginx" ] || echo "${YELLOW}[git-server-wizard] I was unable to find your nginx directory. This could be because you don't have nginx on this system. Please copy .git-http-backend.conf.temp to /etc/nginx/git-http-backend.conf or wherever you nginx config is. Remember to remove the .temp suffix when you copy${NC}"
+[ -d "/etc/nginx" ] || echo "${YELLOW}[git-server-wizard] I was unable to find your nginx directory. This could be because you don't have nginx on this system. Please copy ./git-http-backend.conf.temp to /etc/nginx/git-http-backend.conf or wherever you nginx config is. Remember to remove the .temp suffix when you copy${NC}"
 
 ## Setup nginx
 
@@ -103,4 +103,14 @@ sed -i 's/<<<PATH_HERE>>>/'$ESCAPED_PATH'/' ./git-nginx.conf.temp
 [ -z "$NGINX_FOUND" ] || mv ./git-nginx.conf.temp /etc/nginx/sites-available/git-nginx.conf
 [ -z "$NGINX_FOUND" ] || ln -s /etc/nginx/sites-available/git-nginx.conf /etc/nginx/sites-enabled/git-nginx.conf
 
-[ -z "$NGINX_FOUND" ] && echo "${YELLOW}[git-server-wizard] I was unable to load your nginx config. Take .git-nginx.conf.temp and place it in your nginx config.${NC}"
+[ -z "$NGINX_FOUND" ] && echo "${YELLOW}[git-server-wizard] I was unable to load your nginx config. Take ./git-nginx.conf.temp and place it in your nginx config.${NC}"
+
+echo
+echo "${GREEN}[git-server-wizard] Your git server has been created at $GIT_HOME.${NC}"
+[ -z "$NGINX_FOUND" ] || echo "${GREEN}[git-server-wizard] nginx for $GIT_SITE_NAME has been set up${NC}"
+[ -z "$NGINX_FOUND" ] && echo "${RED}[git-server-wizard] I was unable to find your nginx config.Copy ./git-http-backend.conf.temp to /etc/nginx/git-http-backend.conf and write the contents of ./git-nginx.conf to the appropriate nginx server config files.${NC}"
+echo
+echo "${YELLOW}If you want https/ssl on this website. Run 'certbot --nginx' and select your git website to generate a certificate. The process is automated and should not require much work"
+echo
+echo "You can now create new repositories with 'sudo stagit-newrepo'. You can add your ssh keys to $GIT_USER/.ssh to get ssh based git access to your repos. The address will be $GIT_USER@<site-name.com>:$GIT_HOME/<repo>.git . If your git repositories are in the git users home directory, you can even avoid long path at the end of that address and use :<repo>.git"
+echo "To edit your website styling and logo, add logo.png and style.css to $WWW_HOME and edit $WWW_HOME/index.html"
