db6ac659ba24e4c69fa37339f00db8d6a2e692de
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-06-23 06:46:46 +0400

    Switched to a config file based system
    The old prompt based system was slow and very stupid because I
    would have to take that input and recreate the config file anyways.
    Its better to start with the config.
    Also, having the config file allows for better in file documentation
    to explain what each value is

--- gitServerWizard.sh
+++ gitServerWizard.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/
 #
 #
 
@@ -8,33 +8,49 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 ## Source files
-. utils.sh
+. ./config.rc
+. ./utils.sh
 
 ## Get stagit, compile and install
 # Don't reinstall, if we already have stagit
-(which stagit && which stagit-index) || getStagit
+echo "[git-server-wizard] Checking id stagit exists..."
+(which stagit >/dev/null 2>&1 && which stagit-index >/dev/null 2>&1) || getStagit
 
-## Variables
-read -p "Name of the 'git' user [git]: " GIT_USER_NAME
-GIT_USER_NAME=${GIT_USER_NAME:-git}
-
-read -p "Abbsolute of your git repositories [/home/$GIT_USER_NAME] :" GIT_HOME_DIR
-GIT_HOME_DIR=${GIT_HOME_DIR:-/home/$GIT_USER_NAME}
-
-read -p "Where do you want to place your git web html files [/var/www/htdocs/git]: " GIT_WEB_DIR
-GIT_WEB_DIR=${GIT_WEB_DIR:-/var/www/htdocs/git}
 
 # Confirm if we got all the right information
-
-echo "This is what you entered as your required config:"
-echo "Git User Name: $GIT_USER_NAME"
-echo "Git Directory: $GIT_HOME_DIR"
-echo "Git Web Pages Directory: $GIT_WEB_DIR"
-confirm "Are you sure this is the config you want [y/n]" || exit 1
+echo "This is what you entered as your required config..."
+echo "Make sure none of these fields are blank..."
+echo
+echo "Git User Name: $GIT_USER"
+echo "Git Directory: $GIT_HOME"
+echo "Git Web Pages Directory: $WWW_HOME"
+echo "Git Clone Base URL: $CLONE_URI"
+echo "Default Repo Owner: $DEFAULT_OWNER"
+echo "Default Repo Description: $DEFAULT_DESCRIPTION"
+confirm "Are you sure this is the config you want [y/N]" || exit 1
+confirm "Are you absolutely sure? [y/N]" || exit 1
 
 ## Make the git user
-
 adduser $GIT_USER_NAME || errorOut
+
+## Make the git dir
+mkdir -p $GIT_HOME
+## Make the HTML dir
+mkdir -p $WWW_HOME
+
+# Give permissions
+# The first of these commands is useless for someone
+# who uses the default config or places the git
+# directory somewhere where git user  has permissions
+chown -R $GIT_USER:$GIT_USER $GIT_HOME
+chown -R $GIT_USER:$GIT_USER $WWW_HOME
+
+
+
+## Copy the config file to the git users home directory
+cp ./config.rc $GIT_HOME
+
+
 su $GIT_USER_NAME || errorOut
 
 
--- utils.sh
+++ utils.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
+TEMP_DIR="./.git-server-wizard.temp.d"
+STAGIT_CLONE_DIR="$TEMP_DIR/.stagit"
+
+cleanup() {
+    rm -rf $TEMP_DIR
+}
+
 errorOut() {
+    cleanup
     echo "[git-server-wizard] An error occured. Please see the previous output to find out what went wrong"
     exit 1
 }
@@ -19,9 +27,11 @@ confirm() {
 }
 
 getStagit() {
+    echo "[git-server-wizard] stagit not found. Downloading and installing..."
     cd
-    git clone git://git.codemadness.org/stagit .stagit.temp.d
+    git clone git://git.codemadness.org/stagit $STAGIT_CLONE_DIR
     cd .stagit.temp.d || errorOut
     sudo apt install make gcc libgit2-dev
     make && make install
+    (which stagit >/dev/null 2>&1 && which stagit >/dev/null 2>&1) || errorOut
 }
