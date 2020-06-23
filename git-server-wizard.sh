d9958425d37c6e3168e4f021f6c4f777f22821c0
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-06-23 07:32:44 +0400

    Added stagit files and template.
    All three files are injected with path to user config.
    Some  more bug fixes in main script.

--- git-server-wizard.sh
+++ git-server-wizard.sh
@@ -1,6 +1,6 @@
-#! /bin/
+#! /bin/sh
 #
-#
+# Author: Dhruv Sharma
 
 if [ $(id -u) -ne 0 ]; then
     echo "git-server-wizard must be run as root... exiting..."
@@ -13,11 +13,12 @@ fi
 
 ## Get stagit, compile and install
 # Don't reinstall, if we already have stagit
-echo "[git-server-wizard] Checking id stagit exists..."
+echo "[git-server-wizard] Checking if stagit exists..."
 (which stagit >/dev/null 2>&1 && which stagit-index >/dev/null 2>&1) || getStagit
 
 
 # Confirm if we got all the right information
+echo
 echo "This is what you entered as your required config..."
 echo "Make sure none of these fields are blank..."
 echo
@@ -31,7 +32,7 @@ confirm "Are you sure this is the config you want [y/N]" || exit 1
 confirm "Are you absolutely sure? [y/N]" || exit 1
 
 ## Make the git user
-adduser $GIT_USER_NAME || errorOut
+grep '$GIT_USER' /etc/passwd >/dev/null || $adduser $GIT_USER || errorOut
 
 ## Make the git dir
 mkdir -p $GIT_HOME
@@ -45,12 +46,13 @@ mkdir -p $WWW_HOME
 chown -R $GIT_USER:$GIT_USER $GIT_HOME
 chown -R $GIT_USER:$GIT_USER $WWW_HOME
 
-
-
 ## Copy the config file to the git users home directory
 cp ./config.rc $GIT_HOME
 
-
-su $GIT_USER_NAME || errorOut
-
+## Modify the temp files so they contain the correct path:
+ESCAPED_HOME=$(printf '%s\n' "$GIT_HOME" | sed -e 's/[]\/$*.^[]/\\&/g');
+sed '/#<<<SOURCE_CONFIG_HERE>>>/s/.*/\. '$ESCAPED_HOME'\/config\.rc/' ./stagit-newrepo > ./stagit-newrepo.temp
+sed '/#<<<SOURCE_CONFIG_HERE>>>/s/.*/\. '$ESCAPED_HOME'\/config\.rc/' ./stagit-gen-index > ./stagit-newrepo.temp
+cp -R ./template ./template.temp/
+sed '/#<<<SOURCE_CONFIG_HERE>>>/s/.*/\. '$ESCAPED_HOME'\/config\.rc/' ./template.temp/post-receive > ./template.temp/post-receive
 
